6941b0b448b801b50b8b0664bc688ff0f7f80042
shaddam alghafiqih <shaddam.albz@gmail.com>
2022-07-27 17:07:14 +0700

    Fix miss setup ChakraUI

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <ChakraProvider>
-    <Component {...pageProps} />;
-  </ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
